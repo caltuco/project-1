7ae2c1ecd34ddd5181c09a4dee250a60079ce3e7
Luis A <someemail@example.com>
2023-04-09 19:39:30 -0400

    [ADD]

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -8,3 +8,15 @@ output "azs" {
     value = module.vpc.azs
   
 }
+
+output "public_ids" {
+
+    value = module.vpc.public_subnets
+  
+}
+
+output "private_ids" {
+
+    value = module.vpc.private_subnets
+  
+}
